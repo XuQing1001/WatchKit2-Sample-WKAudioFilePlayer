b88f786e402b5b126e8a69a3f4ca49ffec2242b7
XuQing1001 <xq2417@gmail.com>
2015-07-12 17:14:13 +0800

    Initial Commit

---
+++ WatchKit2-Sample-WKAudioFilePlayer WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  WatchKit2-Sample-WKAudioFilePlayer WatchKit Extension
+//
+//  Created by XuQing on 15/7/12.
+//  Copyright © 2015年 XuQing1001. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
