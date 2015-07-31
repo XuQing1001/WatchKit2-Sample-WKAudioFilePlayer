93027ffd58a4f149975e71c6c9f46a661c74dc2d
XuQing1001 <xq2417@gmail.com>
2015-07-31 21:16:23 +0800

    u

--- WatchKit2-Sample-WKAudioFilePlayer WatchKit Extension/InterfaceController.swift
+++ WatchKit2-Sample-WKAudioFilePlayer WatchKit Extension/InterfaceController.swift
@@ -12,10 +12,29 @@ import Foundation
 
 class InterfaceController: WKInterfaceController {
 
+    @IBOutlet var label: WKInterfaceLabel!
+    var singlePlayer: WKAudioFilePlayer!
+    var queuePlayer: WKAudioFileQueuePlayer!
+    
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
         
         // Configure interface objects here.
+        label.setText("")
+        singlePlayer = WKAudioFilePlayer(playerItem: createPlayerItem("cow", ofType: "mp3"))
+        
+        var playerItems = [WKAudioFilePlayerItem]()
+        playerItems.append(createPlayerItem("cow", ofType: "mp3"))
+        playerItems.append(createPlayerItem("hen", ofType: "mp3"))
+        queuePlayer = WKAudioFileQueuePlayer(items: playerItems)
+    }
+
+    func createPlayerItem(name : String, ofType : String) -> WKAudioFilePlayerItem {
+        let filePath = NSBundle.mainBundle().pathForResource(name, ofType: ofType)! // 搜索音频文件地址
+        let fileUrl = NSURL.fileURLWithPath(filePath) // 获得URL
+        let asset = WKAudioFileAsset(URL: fileUrl) // 用URL创建WKAudioFileAsset对象
+        let playerItem = WKAudioFilePlayerItem(asset: asset)// 用WKAudioFileAsset对象来创建WKAudioFileAsset对象
+        return playerItem
     }
 
     override func willActivate() {
@@ -27,5 +46,28 @@ class InterfaceController: WKInterfaceController {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
-
+    
+    @IBAction func playSingleFile() {
+        switch singlePlayer.status {
+        case .ReadyToPlay:
+            label.setText("单曲准备就绪")
+            singlePlayer.play()
+        case .Failed:
+            label.setText("单曲播放失败")
+        case .Unknown:
+            label.setText("单曲未知错误")
+        }
+    }
+    
+    @IBAction func playMutiFiles() {
+        switch queuePlayer.status {
+        case .ReadyToPlay:
+            label.setText("列表准备就绪")
+            queuePlayer.play()
+        case .Failed:
+            label.setText("列表播放失败")
+        case .Unknown:
+            label.setText("列表未知错误")
+        }
+    }
 }
